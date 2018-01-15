9fb5763e9eece55d2be1c0afbd058355646c8340
Eunbin Bak <peb3978@naver.com>
2018-01-15 12:09:49 -0500

    초기 커밋.

---
+++ Charting/Program.fs
@@ -0,0 +1,20 @@
+﻿
+open System
+open System.Windows.Forms
+open FSharp.Charting
+
+let sigmoid x =
+    1.0 / (1.0 + Math.E ** - x)
+
+let swish x =
+    x / (1.0 + Math.E ** -x)
+
+[<EntryPoint>]
+let main argv =
+    let first = [ for x in -10.0 .. 10.0 -> (x, swish x) ]
+    let second = [ for x in -10.0 .. 10.0 -> (x, sigmoid x) ]
+    let chart = (Chart.Combine [ Chart.Line (first, Name = "Swish"); Chart.Line (second, Name = "Sigmoid") ]).ShowChart ()
+
+    Application.Run chart
+
+    0
